a2b147f6d725737fa5c96402463e3756a8770f31
davaatech <davaa.tech@gmail.com>
2025-01-22 16:36:58 +1100

    Banner and Navbar Portfolio

---
+++ src/components/Banner.js
@@ -0,0 +1,63 @@
+import {Container, Row, Col} from "react-bootstrap" 
+import { ArrowRightCircle} from "react-bootstrap-icons"
+import headingImg from "../assets/img/header-img.svg"
+import { useEffect, useState } from "react";
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = [ "Web Developer", "Software developer", "Software Engineer" ];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(250 - Math.random() * 100);
+    const period = 1500;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+        return () => { clearInterval(ticker) };
+    }, [text])
+    
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText (updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta/2)
+        }
+        if (!isDeleting &&updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+            } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setLoopNum(loopNum + 1);
+                setDelta(500);
+            }
+
+    }
+    
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className= 'align-items-center'>
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Well, hello there!</span>
+                        <h1>{'I am a '}</h1>
+                        <h1><span className="wrap">{text}</span></h1>
+                        <p className="description">You can find some of my work here.</p>
+                        <a href="#" className="btn btn-primary">Let's Connect <ArrowRightCircle size={25}/></a>
+
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headingImg} alt="heading image" className="heading-image" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] =useState('home');
@@ -24,30 +27,34 @@ export const NavBar = () => {
         }
     }, [])
 
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className={ scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="#home">
-            <img src={''} alt='Logo' />
+          <Navbar.Brand className='navbar-brand' href="/">
+            <img src={logo} alt='Logo' />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Projects</Nav.Link>
-              <Nav.Link href="#link">Skills</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#home" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#projects" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#skills" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#About" className={ activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
             </Nav>
             <span className='navbar-text'>
                 <div className='social-icon'>
                     <a href='https://www.facebook.com/your-profile' target='_blank'>
-                        <img src='' />
+                        <img src={navIcon1} alt=""/>
                     </a>
                     <a href='https://www.instagram.com/your-profile' target='_blank'>
-                        <img src='' />
+                        <img src={navIcon2} alt=""/>
                     </a>
                     <a href='' target='_blank' >
-                        Linked In
+                        <img src={navIcon3} alt=""/>
                     </a>
                 </div>
 
