9d623d2cbdfb427133292bd5d62ec267f52af8a9
davaatech <davaa.tech@gmail.com>
2025-01-24 12:51:41 +1100

    Image css

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,11 +2,13 @@ import {Col} from "react-bootstrap";
 
 export const ProjectCard = ({title, description, imgUrl}) => {
     return (
-        <Col sm={6} md={4}>
-            <img src={imgUrl} alt={title} />
+        <Col size={12} sm={6} md={4}>
+            <div className="proj-imgbx">
+            <img src={imgUrl} alt={title} className="proj-img"/>
             <div className="proj-txtx">
             <h4> {title} </h4>
             <span>{description}</span>
+            </div>
             </div>
         </Col>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import projImg1 from "../assets/img/eqthem.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
@@ -11,8 +11,8 @@ export const Projects = () => {
 
     const projects = [
         {
-          title: "Business Startup",
-          description: "Design & Development",
+          title: "EQTHEM",
+          description: "Empowering men in service orineted industry",
           imgUrl: projImg1,
         },
         {
@@ -28,7 +28,7 @@ export const Projects = () => {
         {
           title: "Business Startup",
           description: "Design & Development",
-          imgUrl: projImg1,
+          imgUrl: projImg3,
         },
         {
           title: "Business Startup",
@@ -65,7 +65,7 @@ export const Projects = () => {
                                 </Nav.Item>
                             </Nav>
                             
-                            <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                            <Tab.Content id="slideInUp">
                                 
                                 <Tab.Pane eventKey="first">
                                     <Row>
